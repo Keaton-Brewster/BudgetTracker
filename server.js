b133784866afddaa26b3eb9edb718c4da5ef4210
Timmeree Estes <timmereeestes@timmerees-mbp.lan>
2021-04-13 10:45:29 -0500

    reverting to init

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const app = express();
 app.use(logger("dev"));
 
 app.use(compression());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
@@ -24,7 +22,6 @@ mongoose.connect("mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
